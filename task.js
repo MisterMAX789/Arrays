fd779b4a2ff24043db0c63734965acc55e790b21
MisterMAX789 <maximotruc@mail.ru>
2022-04-14 01:20:25 +0300

    initial commit

---
+++ task.js
@@ -0,0 +1,10 @@
+function compareArrays(arr1, arr2) {
+
+  return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])
+  
+}
+
+function advancedFilter(arr) {
+
+  return arr.filter(item => item%3 == 0).filter(item => item > 0).map(item => item * 10); 
+}
